6b910180c9e1bf5ee2e2157f935fb63391d585d6
SAMRAT DUTTA <trexgodmode777@gmail.com>
2025-02-09 22:55:27 +0530

    Update AddBook.jsx

--- frontend/src/pages/AddBook.jsx
+++ frontend/src/pages/AddBook.jsx
@@ -49,7 +49,7 @@ const AddBook = () => {
   const onSubmit = (values) => {
     setIsLoading(true);
     let promise = axios.post(
-      `https://book-world-main-backend.onrender.com/books`,
+      `${import.meta.env.VITE_BACKEND_URL}/books`,
       values,
       {
         headers: {

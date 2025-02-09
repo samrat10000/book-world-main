a50d3f6eb02f7754cc2ef8107e9e36d7db206170
SAMRAT DUTTA <trexgodmode777@gmail.com>
2025-02-09 22:57:31 +0530

    Update AddBook.jsx

--- frontend/src/pages/AddBook.jsx
+++ frontend/src/pages/AddBook.jsx
@@ -1,68 +1,200 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Sidebar from "@/components/Sidebar";
-import useUserData from "@/hooks/useUserData";
-import { useGoogleOneTapLogin } from "@react-oauth/google";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { Textarea } from "@/components/ui/textarea";
+import { Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { bookSchema } from "@/schema";
+import SelectGenreCombobox from "@/components/SelectGenreCombobox";
+import genres from "@/utilities/genres";
+import { useSetRecoilState } from "recoil";
+import { pageTitleAtom } from "@/atoms/meta";
 import { toast } from "sonner";
 
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useUserData();
-  useEffect(() => {
-    const logVisit = async () => {
-      const userAgent = navigator.userAgent;
-      const { data } = await axios.post(
-        `${import.meta.env.VITE_BACKEND_URL}/log-visit`,
-        {
-          userAgent,
-        }
-      );
-      console.log(`Total unique visitors: ${data.totalVisitors}.`);
-      console.log(`You have visited ${data.totalVisits} time(s).`);
-    };
-    logVisit();
-  }, []);
-
-  useGoogleOneTapLogin({
-    onSuccess: async (credentialResponse) => {
-      let promise = axios.post(
-        `${import.meta.env.VITE_BACKEND_URL}/users/google-auth`,
-        {
-          token: credentialResponse.credential,
-          auth_method: "google",
-        }
-      );
-      toast.promise(promise, {
-        loading: "Loading...",
-        success: (response) => {
-          const { token } = response.data;
-          localStorage.setItem("token", token);
-          setIsLoggedIn(true);
-          return response.data.message;
-        },
-        error: (error) => error.response.data.message,
-      });
+const AddBook = () => {
+  const setPageTitle = useSetRecoilState(pageTitleAtom);
+  useEffect(() => setPageTitle("Add Book"), []);
+  const form = useForm({
+    resolver: zodResolver(bookSchema),
+    defaultValues: {
+      title: "",
+      author: "",
+      description: "",
+      genre: [],
+      image: "",
+      year_published: new Date().getFullYear(),
     },
-    auto_select: true,
-    disabled: isLoggedIn,
   });
+  const navigate = useNavigate();
+  const fileRef = form.register("file");
+  const [isLoading, setIsLoading] = useState(false);
+  const onSubmit = (values) => {
+    setIsLoading(true);
+    let promise = axios.post(
+      `${import.meta.env.VITE_BACKEND_URL}/books`,
+      values,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+
+    toast.promise(promise, {
+      loading: "Loading...",
+      success: (response) => {
+        navigate("/books/");
+        return response.data.message;
+      },
+      error: (error) => error.response.data.message,
+      finally: () => setIsLoading(false),
+    });
+  };
 
   return (
-    <div className="flex min-h-dvh w-full flex-col dark:bg-zinc-950 dark:text-zinc-50">
-      <Sidebar />
-
-      <div className="flex flex-col min-h-dvh sm:pl-14">
-        <Header />
-        <Outlet />
-
-        <div className="flex-1"></div>
-
-        <Footer />
-      </div>
+    <div className="grid flex-1 gap-4 p-4 sm:px-6 md:gap-8">
+      <Card className="w-full max-w-xl mx-auto rounded-lg shadow-md overflow-hidden">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <CardHeader className="border-b border-slate-200 dark:border-zinc-800">
+              <CardTitle>Add Book</CardTitle>
+            </CardHeader>
+            <CardContent className="grid sm:grid-cols-3 gap-3 py-5">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem className="space-y-0 sm:col-span-3">
+                    <FormLabel className="text-left">Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Title of the Book" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="author"
+                render={({ field }) => (
+                  <FormItem className="space-y-0 sm:col-span-2">
+                    <FormLabel className="text-left">Author</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Author of the Book" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="year_published"
+                render={({ field }) => (
+                  <FormItem className="space-y-0">
+                    <FormLabel className="text-left">Year</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Year Published"
+                        type="number"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem className="space-y-0 sm:col-span-3">
+                    <FormLabel className="text-left">Description</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Enter a brief summary of the book..."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="genre"
+                render={({ field }) => (
+                  <FormItem className="space-y-0 sm:col-span-3">
+                    <FormLabel className="text-left">Genre</FormLabel>
+                    <FormControl>
+                      <SelectGenreCombobox
+                        options={genres}
+                        name="Genre"
+                        form={form}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="image"
+                render={({ field }) => {
+                  return (
+                    <FormItem className="space-y-0 sm:col-span-2">
+                      <FormLabel>Image</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="file"
+                          placeholder="Book Thumbnail"
+                          {...fileRef}
+                          onChange={(event) => {
+                            field.onChange(
+                              event.target?.files?.[0] ?? undefined
+                            );
+                          }}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+            </CardContent>
+            <CardFooter className="border-t border-slate-200 px-6 py-4 dark:border-zinc-800">
+              {isLoading ? (
+                <Button disabled>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Please wait
+                </Button>
+              ) : (
+                <Button type="submit">Submit</Button>
+              )}
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
     </div>
   );
-}
+};
 
-export default App;
+export default AddBook;

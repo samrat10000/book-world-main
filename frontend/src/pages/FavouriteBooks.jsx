de953c6638ade762466a803837609d4f09c49c23
SAMRAT DUTTA <trexgodmode777@gmail.com>
2025-02-09 22:59:23 +0530

    Update FavouriteBooks.jsx

--- frontend/src/pages/FavouriteBooks.jsx
+++ frontend/src/pages/FavouriteBooks.jsx
@@ -16,7 +16,7 @@ const FavouriteBooks = () => {
   useEffect(() => {
     localStorage.getItem("token") &&
       axios
-        .get(`https://book-world-main-backend.onrender.com/users/favourites`, {
+        .get(`${import.meta.env.VITE_BACKEND_URL}/users/favourites`, {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
